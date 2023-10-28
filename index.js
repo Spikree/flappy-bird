5fe32ed7391147e4a57727f9adeff1d9764f58bf
Spikey <138967412+Spikree@users.noreply.github.com>
2023-10-28 20:32:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ document.addEventListener('DOMContentLoaded' , () => {
     let birdLeft = 220
     let birdBottom = 200
     let gravity = 2
+    let isGameOver = false
 
     function startGame() {
         birdBottom -= gravity
         bird.style.bottom = birdBottom + 'px'
         bird.style.left = birdLeft + 'px'
     }
-    let timerId = setInterval(startGame, 20)
+    let gameTimerId = setInterval(startGame, 20)
 
     function control(e){
         if (e.keyCode === 32){
@@ -30,4 +31,40 @@ document.addEventListener('DOMContentLoaded' , () => {
 
     document.addEventListener('keyup', control)
     document.addEventListener('mouseup', jump)
+
+    function generateObstacle() {
+        let obstacleLeft = 500
+        let randomHeight = Math.random() * 60
+        let obstacleBottom = randomHeight
+        const obstacle = document.createElement('div')
+        if (!isGameOver) obstacle.classList.add('obstacle')
+        gameDisplay.appendChild(obstacle)
+        obstacle.style.left = obstacleLeft + 'px'
+        obstacle.style.bottom = obstacleBottom + 'px'
+
+        function moveObstacle() {
+            obstacleLeft -= 2
+            obstacle.style.left = obstacleLeft + 'px'
+
+            if(obstacleLeft === -60){
+                clearInterval(timerId)
+                gameDisplay.removeChild(obstacle)
+            }
+            if (obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 ||
+                birdBottom === 0 ) [
+                gameOver()
+            ]
+        }
+        let timerId = setInterval(moveObstacle, 20)
+        if(!isGameOver) setTimeout(generateObstacle, 2500)
+    }
+    generateObstacle()
+
+    function gameOver() {
+        clearInterval(gameTimerId)
+        console.log('game over')
+        isGameOver = true
+        document.removeEventListener('keyup', control)
+        document.removeEventListener('mouseup', jump)
+    }
 })
