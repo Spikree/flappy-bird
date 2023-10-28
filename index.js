43813432dd327b0dbb1b12deaaf581ac5902634b
Spikey <138967412+Spikree@users.noreply.github.com>
2023-10-28 21:06:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded' , () => {
     let birdBottom = 200
     let gravity = 2
     let isGameOver = false
+    let gap = 430
 
     function startGame() {
         birdBottom -= gravity
@@ -37,22 +38,32 @@ document.addEventListener('DOMContentLoaded' , () => {
         let randomHeight = Math.random() * 60
         let obstacleBottom = randomHeight
         const obstacle = document.createElement('div')
-        if (!isGameOver) obstacle.classList.add('obstacle')
+        const topObstacle = document.createElement('div')
+        if (!isGameOver) {
+            obstacle.classList.add('obstacle')
+            topObstacle.classList.add('topObstacle')
+        }
         gameDisplay.appendChild(obstacle)
+        gameDisplay.appendChild(topObstacle)
         obstacle.style.left = obstacleLeft + 'px'
+        topObstacle.style.left = obstacleLeft + 'px'
         obstacle.style.bottom = obstacleBottom + 'px'
+        topObstacle.style.bottom = obstacleBottom + gap + 'px'
 
         function moveObstacle() {
             obstacleLeft -= 2
             obstacle.style.left = obstacleLeft + 'px'
+            topObstacle.style.left = obstacleLeft + 'px'
 
             if(obstacleLeft === -60){
                 clearInterval(timerId)
                 gameDisplay.removeChild(obstacle)
+                gameDisplay.removeChild(topObstacle)
             }
-            if (obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 ||
+            if (obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 && (birdBottom < obstacleBottom +155 || birdBottom > obstacleBottom + gap -200)||
                 birdBottom === 0 ) [
-                gameOver()
+                gameOver(),
+                clearInterval(timerId)
             ]
         }
         let timerId = setInterval(moveObstacle, 20)
